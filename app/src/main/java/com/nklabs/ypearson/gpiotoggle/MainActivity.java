cad1615514322361d3cf07585640345aaeef61ba
Yvan Pearson <ypearson@nklabs.com>
2015-08-28 15:44:05 -0400

    Initial commit

---
+++ app/src/main/java/com/nklabs/ypearson/gpiotoggle/MainActivity.java
@@ -0,0 +1,59 @@
+package com.nklabs.ypearson.gpiotoggle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {System.loadLibrary("gpio");}
+    public native String  stringFromJNI();
+    public native String  unimplementedStringFromJNI();
+
+
+    private static final String TAG = "nklabs";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "OnCreate");
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        textView1.setText("GPIO test app launched.");
+        textView1.setText(stringFromJNI());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
